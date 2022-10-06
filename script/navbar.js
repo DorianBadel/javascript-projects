ecedefb2719efa4e84ddfc53c2afb6c5ea7002fd
Dorian Badel <DorianBadel@protonmail.com>
2022-10-06 21:44:43 +0200

    Short method of hiding the navbar

--- script/navbar.js
+++ script/navbar.js
@@ -8,7 +8,5 @@ const navToggle = document.querySelector('.nav-toggle');
 const links = document.querySelector(".links");
 
 navToggle.addEventListener('click',()=>{
-  links.classList.contains('show-links')
-  ? links.classList.remove('show-links')
-  : links.classList.add('show-links');
+  links.classList.toggle('show-links');
 });
