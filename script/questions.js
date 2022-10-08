e3322babd94617f8e3a8fb2f7fc72ae32fa2df77
DorianBadel <DorianBadel@protonmail.com>
2022-10-08 15:36:15 +0200

    Questions project done

---
+++ script/questions.js
@@ -0,0 +1,25 @@
+/*
+const btns = document.querySelectorAll(".question-btn");
+
+btns.forEach(btn => {
+    btn.addEventListener('click',(e)=>{
+        const question = e.currentTarget.parentElement.parentElement;
+        question.classList.toggle("show-text");
+    });
+});*/
+
+const questions = document.querySelectorAll(".question");
+
+questions.forEach(question =>{
+    const btn = question.querySelector(".question-btn");
+    btn.addEventListener('click', ()=>{
+        questions.forEach(q =>{
+            if(q !== question){
+                q.classList.remove('show-text');
+            }
+        });
+
+        question.classList.toggle('show-text');
+    });
+});
+
